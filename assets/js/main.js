f3e3a899c945f12ecb1701c5a2ac6888eab76415
JumpLink <jumplink@gmail.com>
2014-12-05 18:56:35 +0100

    cleanup und animation wiederholen

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ var getPageSize = function () {
 }
 
 /**
- * Alle benötigten SVG Dateien laden
+ * Alle benötigten SVG Dateien angular.identity()
  */
 var prepare = function (callback) {
   var cars = [], house;
@@ -23,8 +23,8 @@ var prepare = function (callback) {
       selector: selector = '#house',
       svg: svg,
       element: svg.select(selector),
-      width: 656,
-      height: 344
+      width: 342,
+      height: 150
     };
     Snap.load("assets/images/folienhaus_cars-01.svg", function (svg) {
       cars.push({
@@ -32,7 +32,7 @@ var prepare = function (callback) {
         svg: svg,
         element: svg.select(selector),
         width: 256,
-        height: 112
+        height: 150
       });
       Snap.load("assets/images/folienhaus_cars-02.svg", function (svg) {
         cars.push({
@@ -47,8 +47,8 @@ var prepare = function (callback) {
             selector: selector = '#car',
             svg: svg,
             element: svg.select(selector),
-            width: 265,
-            height: 109
+            width: 365,
+            height: 150
           });
           Snap.load("assets/images/folienhaus_cars-04.svg", function (svg) {
             cars.push({
@@ -84,6 +84,64 @@ var prepare = function (callback) {
   });
 }
 
+// TODO center punkt finden
+var wheelAnimation = function (level0) {
+  var wheel = level0.select('#wheel1');
+  wheel.stop().animate(
+    { transform: 'r90,256,256'}, // Basic rotation around a point. No frills.
+    1000, // drehgeschwindigkeit
+    function() { 
+      wheel.attr({ transform: 'rotate(0 256 256)'}); // Reset the position of the wheel.
+      wheelAnimation(); // Repeat this animation so it appears infinite.
+    }
+  );
+}
+
+/*
+ * Ins Haus fahren, warten und weiter fahren.
+ * callback wird am Ende der Animtion aufgerufen.
+ */
+var driveAnimation = function (level0, car, callback) {
+  var driveInTime = 4;    // seconds
+  var stayTime = 3;       // seconds
+  var driveOutTime = 2;   // seconds
+  var x = 0;
+
+  level0.append(car.svg);   // cars[0].svg wird automatisch mittig in der SVG plaziert, da width: 100%
+
+  // wheelAnimation(level0);
+
+  // Auto startet außerhalb der Seite
+  x = (((getPageSize().width + car.width) / 2 ) * -1) + 200 // berechnet den linken Außenbereich der Seite + 200px zur Sicherheit (falls das Browserfenster vergrößert wird)
+  move('#car').x(x).duration('0s').end(function(){
+    // Auto fährt ins Haus 
+    x = 0; // Null ist center innerhalt der SVG
+    move('#car').ease('out').x(x).duration(driveInTime+'s').end(function () {
+      // Auto bleibt 3000 ms stehen
+      setTimeout(function(){
+        // Auto fährt wieder raus
+        x = ((getPageSize().width + car.width) / 2 ) + 200 // berechnet den rechten Außenbereich der Seite + 200px zur Sicherheit (falls das Browserfenster vergrößert wird)
+        move('#car').ease('in').x(x).duration(driveOutTime+'s').end(function () {
+          callback();
+        });
+      }, (stayTime * 1000));
+    });
+  });
+}
+
+/*
+ * Animationsinterval, ruft immer und immer wieder die Animation auf (rekursiv).
+ */
+var driveAnimationInterval = function (level0, cars) {
+  var duration = 4;       // seconds, Zeit die zwischen den Animationen vergeht
+  console.log("neuer Interval");
+  driveAnimation(level0, cars[0], function () {
+    setTimeout(function(){
+      driveAnimationInterval(level0, cars);
+    }, (duration * 1000));
+  });
+}
+
 /**
  * Darauf warten, dass die Seite vollständig geladen wurde.
  * Für das dynamische einbinden und manipulieren von Vektorgrafiken wird Snap.svg verwendet: http://snapsvg.io/
@@ -94,36 +152,13 @@ $(document).ready(function() {
   var level1 = Snap("#level1"); // level 1 - auf gleicher Ebene wie das Haus bzw. Ebene für das Haus
   var cars = [];
   var house;
-  var driveInTime = 4; // seconds
-  var stayTime = 3; // seconds
-  var driveOutTime = 2; // seconds
-  var x = 0;
-
   prepare(function (error, data) {
     house = data.house;
     cars = data.cars;
     
-    level1.append(house.svg);     // Car wird automatisch mittig in der SVG plaziert, da width: 100%
-    level0.append(cars[0].svg);   // Car wird automatisch mittig in der SVG plaziert, da width: 100%
+    level1.append(house.svg);     // house.svg wird automatisch mittig in der SVG plaziert, da width: 100%
 
-    // Auto startet außerhalb der Seite
-    x = (((getPageSize().width + cars[0].width) / 2 ) * -1) + 200 // berechnet den linken Außenbereich der Seite + 200px zur Sicherheit (falls das Browserfenster vergrößert wird)
-    move('#car').x(x).duration('0s').end(); 
-
-    // Auto fährt ins Haus 
-    x = 0;
-    move('#car').ease('out').x(x).duration(driveInTime+'s').end();
-
-    // Auto bleibt 3000 ms stehen
-    setTimeout(function(){
-
-      // Auto fährt wieder raus
-      x = ((getPageSize().width + cars[0].width) / 2 ) + 200 // berechnet den rechten Außenbereich der Seite + 200px zur Sicherheit (falls das Browserfenster vergrößert wird)
-      move('#car').ease('in').x(x).duration(driveOutTime+'s').end();
-
-    }, (driveInTime * 1000) + (stayTime * 1000));
-
-
+    driveAnimationInterval(level0, cars);
 
   });
 
